134a0b900845e6ac6d98660028549ddad51d1ff6
Evgeny Shikanov <sheekanov@gmail.com>
2018-06-02 15:50:42 +0300

    php Code Sniffer's errors fixed

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-CONST testConstanta = 'asd';
+const TESTCONSTANTA = 'asd';
 $user_name = "Igor";
-function show_something(){ RETURN 'SOMETHING';}
-if (1==1) echo 'hi';
+function show_something()
+{
+    return 'SOMETHING';
+}
+if (1==1) {
+    echo 'hi';
+}
