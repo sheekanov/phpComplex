d9c54b15b3ea8bfe727bff5d173f9e0c48da6dc8
Evgeny Shikanov <sheekanov@gmail.com>
2018-06-12 22:48:55 +0300

    ВП 1 отправка формы переделала с использованием serialize()

--- WP1/js/form.js
+++ WP1/js/form.js
@@ -3,56 +3,23 @@ $(document).ready(function(){
     //отправка формы
     (function(){
         $('#form_submit').on('click', function(e) {
-            e.preventDefault();
 
-            var name = $('#name').val();
-            var tel = $('#tel').val();
-            var email = $('#email').val();
-            var street = $('#street').val();
-            var house = $('#house').val();
-            var corp = $('#corp').val();
-            var appt = $('#appt').val();
-            var floor = $('#floor').val();
-            var comment = $('#comment').val();
-            var callback = $('#callback:checked').val();
-            var payment = $('input[name=payment]:checked').val();
+            e.preventDefault();
+            var data = $('#order-form').serialize();
 
             $.ajax({
                 url: 'form.php',
                 method: 'POST',
-                dataType: 'json',
-                data: {
-                    name: name,
-                    tel: tel,
-                    email: email,
-                    street: street,
-                    house: house,
-                    corp: corp,
-                    appt: appt,
-                    floor: floor,
-                    comment: comment,
-                    callback: callback,
-                    payment: payment
-                }
+                dataType: 'html',
+                data: data,
             }).done(function(response) {
-                if (response.success){ //если скрипт отработал удачно, очищаем поля ввода и показываем попап с успехом. В консоль пишем сообщение сервера.
-                    $('#name').val('');
-                    $('#tel').val('');
-                    $('#email').val('');
-                    $('#street').val('');
-                    $('#house').val('');
-                    $('#corp').val('');
-                    $('#appt').val('');
-                    $('#floor').val('');
-                    $('#comment').val('');
-                    $('input[name=payment]').prop('checked', false);
-                    $('#callback').prop('checked', false);
-
-                    console.log(response.message);
-
+                var jsoned = JSON.parse(response);
+                if (jsoned.success){ //если скрипт отработал удачно, очищаем поля ввода и показываем попап с успехом. В консоль пишем сообщение сервера.
+                    console.log(jsoned.message);
                     $('#success').addClass("popup__active");
+                    $('#order-form').reset();
                 } else { //если скрипт отработал с ошибкой, показываем попап с ошибкой. В консоль пишем сообщение сервера.
-                    console.log(response.message);
+                    console.log(jsoned.message);
                     $('#error').addClass("popup__active");
                 }
             })
